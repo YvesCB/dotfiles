c119b6274cdaea99d7d4e13fdbbe6e33af02655b
YvesCB <y.bruckmayer@gmail.com>
2024-03-07 21:14:16 +0100

    fixed mistake

--- nvim/lua/yves/plugins/todo-comments.lua
+++ nvim/lua/yves/plugins/todo-comments.lua
@@ -6,8 +6,12 @@ return {
 		opts = { signs = false },
 		config = function()
 			local todo = require("todo-comments")
-			vim.keymap.set("n", "]t", todo.jump_next(), { desc = "Jump to next TODO item" })
-			vim.keymap.set("n", "[t", todo.jump_previous(), { desc = "Jump to previous TODO item" })
+			vim.keymap.set("n", "]t", function()
+				todo.jump_next()
+			end, { desc = "Jump to next TODO item" })
+			vim.keymap.set("n", "[t", function()
+				todo.jump_previous()
+			end, { desc = "Jump to previous TODO item" })
 		end,
 	},
 }
