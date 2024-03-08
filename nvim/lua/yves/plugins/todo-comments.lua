958e3909fceaebc8dc2c6c466a2b5a70be492fc1
YvesCB <y.bruckmayer@gmail.com>
2024-03-08 20:31:53 +0100

    fixed todo

--- nvim/lua/yves/plugins/todo-comments.lua
+++ nvim/lua/yves/plugins/todo-comments.lua
@@ -1,17 +1,16 @@
 return {
-	{
-		"folke/todo-comments.nvim",
-		event = "VimEnter",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = { signs = false },
-		config = function()
-			local todo = require("todo-comments")
-			vim.keymap.set("n", "]t", function()
-				todo.jump_next()
-			end, { desc = "Jump to next TODO item" })
-			vim.keymap.set("n", "[t", function()
-				todo.jump_previous()
-			end, { desc = "Jump to previous TODO item" })
-		end,
-	},
+	"folke/todo-comments.nvim",
+	event = "VimEnter",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local todo = require("todo-comments")
+		vim.keymap.set("n", "]t", function()
+			todo.jump_next()
+		end, { desc = "Jump to next TODO item" })
+		vim.keymap.set("n", "[t", function()
+			todo.jump_previous()
+		end, { desc = "Jump to previous TODO item" })
+
+		todo.setup({ signs = false })
+	end,
 }
