607554a98cc5da01f39e81f7435b1fba700acd47
YvesCB <y.bruckmayer@gmail.com>
2025-09-05 20:49:22 +0200

    changing some tabsize stuff

--- nvim/.config/nvim/lua/yves/core/filetype.lua
+++ nvim/.config/nvim/lua/yves/core/filetype.lua
@@ -2,22 +2,33 @@
 
 -- Automatically set the line wrap and spell but only in pure text files
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	pattern = { "*.txt", "*.md", "*.tex", "*.typ" },
-	desc = "Set wordwrap and spell when in 'txt', 'md', 'tex' or 'typst' files.",
-	group = vim.api.nvim_create_augroup("linewrap-in-text-files", { clear = true }),
-	callback = function()
-		vim.opt_local.wrap = true
-		vim.cmd("setlocal spell")
-	end,
+  pattern = { "*.txt", "*.md", "*.tex", "*.typ" },
+  desc = "Set wordwrap and spell when in 'txt', 'md', 'tex' or 'typst' files.",
+  group = vim.api.nvim_create_augroup("linewrap-in-text-files", { clear = true }),
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.cmd("setlocal spell")
+  end,
 })
 
 -- Automatically set the shiftwidth and tabstop for web things
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+-- 	pattern = { "*.html", "*.css", "*.js", "*.ts", "*.svelte", "*.jsx", "*.tsx" },
+-- 	desc = "Set the tabstop and shiftwidth to 2 for web dev files.",
+-- 	group = vim.api.nvim_create_augroup("tabstop-and-shiftwidth-for-web-dev", { clear = true }),
+-- 	callback = function()
+-- 		vim.o.tabstop = 2 -- 2 spaces for tabs (prettier default is 2)
+-- 		vim.o.shiftwidth = 2 -- 2 spaces for indent width
+-- 	end,
+-- })
+
+-- I like tapstop = 4 in C and Rust (and some extra)
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	pattern = { "*.html", "*.css", "*.js", "*.ts", "*.svelte", "*.jsx", "*.tsx" },
-	desc = "Set the tabstop and shiftwidth to 2 for web dev files.",
-	group = vim.api.nvim_create_augroup("tabstop-and-shiftwidth-for-web-dev", { clear = true }),
-	callback = function()
-		vim.o.tabstop = 2 -- 2 spaces for tabs (prettier default is 2)
-		vim.o.shiftwidth = 2 -- 2 spaces for indent width
-	end,
+  pattern = { "*.c", ".rs", ".toml", ".json" },
+  desc = "Set the tabstop and shiftwidth to 4.",
+  group = vim.api.nvim_create_augroup("tabstop-and-shiftwidth-four", { clear = true }),
+  callback = function()
+    vim.o.tabstop = 4    -- 4 spaces for tabs
+    vim.o.shiftwidth = 4 -- 4 spaces for indent width
+  end,
 })
